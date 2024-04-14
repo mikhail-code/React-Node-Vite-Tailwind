f51c5cd5191315542254ea45b47b2c442ebe2dd9
Mikhail Kravchenko <km.code@proton.me>
2024-04-14 20:31:46 +0300

    Added Headless UI support, added headless UI example

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css'; // Optional for styling
+import Tabs from './assets/tabs'
+
 
 console.log("API endpoint:", '/api/data'); // Log the endpoint
 
@@ -19,13 +21,15 @@ function App() {
   
     fetchData();
   }, []);
-
+// <p className="text-3xl font-bold bg-red-400 underline">Loading data...</p>
   return (
     <div className="App">
       {serverData ? (
         <p>Data from server: {serverData.message}</p>
       ) : (
-        <p className="text-3xl font-bold bg-red-400 underline">Loading data...</p>
+        <div>
+          <Tabs />
+        </div>
       )}
     </div>
   );
